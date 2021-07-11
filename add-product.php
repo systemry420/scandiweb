5baa21a4a3541986f51163ceb196bb5da9006f31
systemry420 <systemry420@gmail.com>
2021-07-11 15:46:58 +0300

    insert product

--- add-product.php
+++ add-product.php
@@ -2,16 +2,35 @@
 
     include './includes/connection.php';
 
-    if (isset($_POST['save'])) {
-        $sku = $_POST['sku'];
-        $name = $_POST['name'];
-        $price = $_POST['price'];
-        $type = $_POST['type'];
+    $sku = $_POST['sku'];
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $type = $_POST['productType'];
+
+    if ($type == "DVD") {
+        $size = $_POST['size'];
     }
-
-
+    else if ($type == "Furniture") {
+        $height = $_POST['height'];
+        $width = $_POST['width'];
+        $length = $_POST['length'];
+    }
+    else if ($type == "Book") {
+        $weight = $_POST['weight'];
+    }
     //todo: validation
 
-    
+    if (isset($_POST['save'])) {
+        $sql = "INSERT INTO `products`(`sku`, `name`, `price`)
+        VALUES ('$sku', '$name', '$price')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    }
     
 ?>
--- includes/connection.php
+++ includes/connection.php
@@ -2,8 +2,9 @@
     $servername = "localhost";
     $username = "root";
     $password = "mqgh7*eZKC5pCm82";
+    $dbname = "product_db";
 
-    $conn = new mysqli($servername, $username, $password);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
